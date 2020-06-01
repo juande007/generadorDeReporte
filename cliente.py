a98766d59c455e6ed893a26a9dfcfcc6e200a80c
juan diego villamarin <juande.ing@gmail.com>
2020-06-01 15:57:52 -0500

    test
    Co-Authored-By: null <NicoVerdugo@users.noreply.github.com>

--- cliente.py
+++ cliente.py
@@ -4,15 +4,14 @@
 #	id_cliente = int(raw_input('Customer (1=Tigo Bolivia 2=Nuevatel ): '))
 
 class cliente():
-
-    def __init__(nombre_cliente, nombre_contacto):
+	def __init__(nombre_cliente, nombre_contacto):
 		self.nombre_cliente = nombre_cliente
  	    self.nombre_contacto = nombre_contacto
 
- def cliente_contacto(self):
-    return "El nombre del cliente es %s y su contacto es %i" % (self.nombre_cliente, self.nombre_contacto)
+	def cliente_contacto(self):
+		return "El nombre del cliente es %s y su contacto es %i" % (self.nombre_cliente, self.nombre_contacto)
 
 def main():
-	print("hola")
+	print("hola2")
 
 def __name__=='__main__':
