31ce9c297cb3351618a4792773dc20b92e945490
NicoVerdugo <nikochap96@gmail.com>
2020-09-11 17:21:51 -0500

    Actualizacion de Frontend
    Se actualiza el frontend y se logra envio de sondas con Issue a clase generador de Word

--- frontend.py
+++ frontend.py
@@ -1,6 +1,8 @@
 import eel, os, random
 import GeneradorDeReporte
 from incidents import Incidents
+# from generador_word import datosReporte
+from generador_word import Reportes
 
 
 
@@ -8,13 +10,23 @@ eel.init('web')
 
 @eel.expose
 def pick_file(comando):
+
+    incidentes = Incidents()
+    mostrarIncidents = incidentes.mostrarIncidents()
+    inc =''
+
     reporte = GeneradorDeReporte
     sondas = reporte.tarea(comando)
     print(comando)
     resultado=''
     for element in sondas:
-        resultado=resultado+"<div class='boton_clientes'>"+element+"</div>"
+        resultado=resultado+"<div><li id="+element+">"+element+"</li>"
+        for issueElmente in mostrarIncidents:
+            resultado=resultado+"<li class='boton_sondas' onclick= adicionarSondaAReporte('"+element+"','"+issueElmente+"') >"+issueElmente+"</li>"
+    resultado=resultado+"</div>"
+
     return resultado
+
 
 @eel.expose
 def clientes():
@@ -22,7 +34,7 @@ def clientes():
     listadoClientes = reportes.listadoDeClientes()
     resultado=''
     for element in listadoClientes:
-        resultado=resultado+"<div class='boton_clientes' onclick=prueba('"+element+"')>"+element+"</div>"
+        resultado=resultado+"<div class='boton_clientes' id="+element+" onclick=buscarCliente('"+element+"')>"+element+"</div>"
 
     print ("el objet es!"+str(type(resultado)))
     print (resultado)
@@ -34,9 +46,17 @@ def listaIncidentes():
     mostrarIncidents = incidentes.mostrarIncidents()
     inc =''
     for element in mostrarIncidents:
-        inc = inc + "<div class='boton_clientes'>"+element+"</div>"
+        inc = inc + "<div class='boton_issues' id="+element+" onclick=adicionarIssueAReporte('"+element+"')>"+element+"</div>"
     print (inc)
     print("entro aqui")
     return inc
+@eel.expose
+def imprimirSonda(datosSonda):
+    print("Resultadp desde python "+str(datosSonda))
+    li = list(datosSonda.split(";"))
+    for element in li:
+        print element
+    reportes = Reportes()
+    datosReporte = reportes.datosReporte(li)
 
 eel.start('frontend.html')
--- generador_word.py
+++ generador_word.py
@@ -1,71 +1,106 @@
 # -*- coding: utf-8 -*-
 import time
-
+import json
 from docx import Document
 from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Inches
 from docx.enum.text import WD_LINE_SPACING
 from docx.shared import Length
 from docx.shared import Pt
+#
+class Reportes:
+# 	def __init__(self):
+# 		self.cliente =''
+	def datosReporte(self, li):
 
-#navegador = str(raw_input('Ejemplo: Ingrese Tigo 3322'))
+		self.lista = li
 
-#contact = str(raw_input('Nombre de Contacto : '))
-#navegador =
+		for element in li:
+			lo = list(element.split(","))
+			for element in lo:
+				print lo[1]
 
-#if navegador
+				
+	def recolectorDatos():
 
-#def leer_tarea():  # Es la tarea que el usuario va a realizar, como escribir:tigo 3322
-id_sonda = int(raw_input('Ingrese el ID de la sonda: '))
-id_cliente = int(raw_input('Customer (1=Tigo Bolivia 2=Nuevatel ): '))
-#leer_tarea()
+		incidente='LatePacket.json'
+		f = open("C:\Users\CORE i3\Documents\GitHub\generadorDeReporte/Incidentes/"+incidente, "r")
+		contenido = f.read()
+		archivoJson = json.loads(contenido)
 
-def run():
-	if id_cliente == 1:
-		print 'Descarga el doc 1'
-		document = Document()
-		style = document.styles['Normal']
-		font = style.font
-		font.name = 'Arial'
-		font.size = Pt(9.1)
-		font.italic = True
+		for lineaArchivoConfig in archivoJson["nuevoIncident"]:
+			analisis  = lineaArchivoConfig["Analisis"]
+			descripcion = lineaArchivoConfig["Descripcion"]
+			workAround = lineaArchivoConfig["Workaround"]
+			rootCause = lineaArchivoConfig["Root Cause"]
+			recomendacion = lineaArchivoConfig["Recomendacion"]
 
-		document.add_paragraph("--------------------------------------------------------------------")
-		document.add_paragraph("1. GENERAL INFO")
-		document.add_paragraph("GEMALTO SUPPORT TEAM - TECHNICAL REPORT")
-		document.add_paragraph("Customer: Tigo Bolivia")
-		document.add_paragraph("Contac Name: Carlos Mercado")
-		document.add_paragraph("Call id: ")
-		document.add_paragraph("Date & Time: " + time.strftime("%x"))
+			print(descripcion)
+			print(analisis)
+			print(rootCause)
 
-		paragraph = document.add_paragraph('')
-		run = paragraph.add_run('Cliente ID: ' + str(id_sonda))
-		run.bold = True
+	def generadorWord(self):
 
-		paragraph = document.add_paragraph('')
-		run = paragraph.add_run('Descripcion: ')
-		run.bold = True
+			id_sonda = int(raw_input('Ingrese el ID de la sonda: '))
+			id_cliente = int(raw_input('Customer (1=Tigo Bolivia 2=Nuevatel ): '))
 
-		document.add_paragraph("--------------------------------------------------------------------")
-		document.add_paragraph("2. DETAILS OF INCIDENT: ")
-		document.add_paragraph("Impacted platform: ")
-		document.add_paragraph("Root Cause: ")
-		document.add_paragraph("Incident description: ")
-		document.add_paragraph("Evidencias: ")
+			if id_cliente == 1:
+				print 'Descarga el doc 1'
+				document = Document()
+				style = document.styles['Normal']
+				font = style.font
+				font.name = 'Arial'
+				font.size = Pt(9.1)
+				font.italic = True
 
-		document.add_paragraph("--------------------------------------------------------------------")
-		document.add_paragraph("3. RESOLUTION")
-		document.add_paragraph("Incident Analysis: ")
-		document.add_paragraph("Workaround: ")
-		document.add_paragraph("Recommendation: ")
-		document.add_paragraph("Additional comments: NA")
-		document.save('Sonda.docx')
+				document.add_paragraph("--------------------------------------------------------------------")
+				document.add_paragraph("1. GENERAL INFO")
+				document.add_paragraph("GEMALTO SUPPORT TEAM - TECHNICAL REPORT")
+				document.add_paragraph("Customer: Tigo Bolivia")
+				document.add_paragraph("Contac Name: Carlos Mercado")
+				document.add_paragraph("Call id: ")
+				document.add_paragraph("Date & Time: " + time.strftime("%x"))
 
-	elif id_cliente > 1:
-		print ('Descarga doc 2')
-		document = Document()
-		document.add_heading("Python Doc Word 2")
-		document.add_paragraph("ID de la sonda: " + str(id_sonda))
-		document.add_paragraph("ID de erros: ")
-		document.save('SondaDePrueba2.docx')
-run()
+				paragraph = document.add_paragraph('')
+				run = paragraph.add_run('Cliente ID: ' + str(id_sonda))
+				run.bold = True
+
+				paragraph = document.add_paragraph('')
+				run = paragraph.add_run('Descripcion: ')
+				run.bold = True
+
+				document.add_paragraph("--------------------------------------------------------------------")
+				document.add_paragraph("2. DETAILS OF INCIDENT: ")
+				document.add_paragraph("Impacted platform: ")
+				document.add_paragraph("Root Cause: ")
+				document.add_paragraph("Incident description: ")
+				document.add_paragraph("Evidencias: ")
+
+				document.add_paragraph("--------------------------------------------------------------------")
+				document.add_paragraph("3. RESOLUTION")
+				document.add_paragraph("Incident Analysis: ")
+				document.add_paragraph("Workaround: ")
+				document.add_paragraph("Recommendation: ")
+				document.add_paragraph("Additional comments: NA")
+				document.save('.\Documents\GitHub\generadorDeReporte\Clientes\Temporal\Reporte_Errores\Sonda.docx')
+
+			elif id_cliente > 1:
+				print ('Descarga doc 2')
+				document = Document()
+				document.add_heading("Python Doc Word 2")
+				document.add_paragraph("ID de la sonda: " + str(id_sonda))
+				document.add_paragraph("ID de erros: ")
+				document.save('SondaDePrueba2.docx')
+
+class DatosReporte:
+	   def __init__(self, sonda, issue):
+			self.sonda = sonda
+			self.issue = issue
+			self.lista = []
+
+def main():
+	# reportes = Reportes()
+	# reportes.generadorWord()
+	datosReporte()
+if __name__=='__main__':
+        main()
